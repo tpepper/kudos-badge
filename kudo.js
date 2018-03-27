d40e5ddb93e28566b8e3b7d03527577374590b75
Tim Pepper <tpepper@vmware.com>
2018-03-27 16:00:25 -0700

    unhard code the repo and commit
    The caller will specify these.
    
    Signed-off-by: Tim Pepper <tpepper@vmware.com>

--- kudo.js
+++ kudo.js
@@ -1,6 +1,5 @@
-repo="tpepper/kudos-badge"
-commit="5bba5d67128e73319deb564144fb1a861697bd24"
-url="https://api.github.com/repos/"+repo+"/commits/"+commit
+//repo and commit come from the caller
+var url="https://api.github.com/repos/"+repo+"/commits/"+commit
 
 fetch(url, {
 	mode: 'cors'
@@ -9,8 +8,6 @@ fetch(url, {
 	return response.json();
 }).then(function(json) {
 	kudoMsg = json.commit.message;
-	console.log(kudoMsg);
-
 	kudoMsg = kudoMsg.replace('^.*\n\n', '');
 	commitMsgJSON = JSON.parse(kudoMsg);
 
